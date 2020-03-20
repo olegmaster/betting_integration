3402354d1fa5b5f90f6a6f8881408f4ac6906349
oleg-divine <oleg.web.pro@gmail.com>
2020-03-20 16:00:36 +0200

    add admin summary action

---
+++ resources/views/admin/summary.blade.php
@@ -0,0 +1,235 @@
+@extends('layouts.admin')
+
+@section('title', 'Сводка')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6 col-xl-3">
+            <div class="card mb-3 widget-content bg-premium-dark">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Общая прибыль</div>
+
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-warning"><span>$14M</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="card mb-3 widget-content bg-arielle-smile">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Прибыль за период</div>
+
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-white"><span>$ 568</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-3">
+            <div class="card mb-3 widget-content bg-night-fade">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Ключей</div>
+
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-white"><span>1896</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-xl-3">
+            <div class="card mb-3 widget-content bg-happy-green">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Пользователей</div>
+
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-dark"><span>46%</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="d-xl-none d-lg-block col-md-6 col-xl-4">
+            <div class="card mb-3 widget-content bg-premium-dark">
+                <div class="widget-content-wrapper text-white">
+                    <div class="widget-content-left">
+                        <div class="widget-heading">Products Sold</div>
+                        <div class="widget-subheading">Revenue streams</div>
+                    </div>
+                    <div class="widget-content-right">
+                        <div class="widget-numbers text-warning"><span>$14M</span></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-lg-6">
+            <div class="mb-3 card">
+
+                <div class="tab-content">
+                    <div class="tab-pane fade active show" id="tab-eg-55">
+
+                        <div class="widget-chart p-3">
+                            <canvas id="chart-horiz-bar"></canvas>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-12 col-lg-6">
+            <div class="mb-3 card">
+                <div class=" card-header">
+                    <div class="card-header-title">
+                        <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
+                        ТОП ПОЛЬЗОВАТЕЛИ
+                    </div>
+                </div>
+                <div class="tab-content">
+                    <div class="tab-pane fade active show" id="tab-eg-55">
+
+                        <div class="widget-chart p-3">
+                            <div class="scroll-area-sm">
+                                <div class="scrollbar-container">
+                                    <ul class="rm-list-borders rm-list-borders-scroll list-group list-group-flush">
+                                        <li class="list-group-item">
+                                            <div class="widget-content p-0">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left mr-3">
+                                                        <img width="42" class="rounded-circle"
+                                                             src="assets/images/avatars/9.jpg" alt="">
+                                                    </div>
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Ella-Rose Henry</div>
+                                                        <div class="widget-subheading">Web Developer</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="font-size-xlg text-muted">
+                                                            <small class="opacity-5 pr-1">$</small>
+                                                            <span>129</span>
+                                                            <small class="text-danger pl-2">
+                                                                <i class="fa fa-angle-down"></i>
+                                                            </small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="widget-content p-0">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left mr-3">
+                                                        <img width="42" class="rounded-circle"
+                                                             src="assets/images/avatars/5.jpg" alt="">
+                                                    </div>
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Ruben Tillman</div>
+                                                        <div class="widget-subheading">UI Designer</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="font-size-xlg text-muted">
+                                                            <small class="opacity-5 pr-1">$</small>
+                                                            <span>54</span>
+                                                            <small class="text-success pl-2">
+                                                                <i class="fa fa-angle-up"></i>
+                                                            </small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="widget-content p-0">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left mr-3">
+                                                        <img width="42" class="rounded-circle"
+                                                             src="assets/images/avatars/4.jpg" alt="">
+                                                    </div>
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Vinnie Wagstaff</div>
+                                                        <div class="widget-subheading">Java Programmer</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="font-size-xlg text-muted">
+                                                            <small class="opacity-5 pr-1">$</small>
+                                                            <span>429</span>
+                                                            <small class="text-warning pl-2">
+                                                                <i class="fa fa-dot-circle"></i>
+                                                            </small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="widget-content p-0">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left mr-3">
+                                                        <img width="42" class="rounded-circle"
+                                                             src="assets/images/avatars/3.jpg" alt="">
+                                                    </div>
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Ella-Rose Henry</div>
+                                                        <div class="widget-subheading">Web Developer</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="font-size-xlg text-muted">
+                                                            <small class="opacity-5 pr-1">$</small>
+                                                            <span>129</span>
+                                                            <small class="text-danger pl-2">
+                                                                <i class="fa fa-angle-down"></i>
+                                                            </small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <div class="widget-content p-0">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left mr-3">
+                                                        <img width="42" class="rounded-circle"
+                                                             src="assets/images/avatars/2.jpg" alt="">
+                                                    </div>
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-heading">Ruben Tillman</div>
+                                                        <div class="widget-subheading">UI Designer</div>
+                                                    </div>
+                                                    <div class="widget-content-right">
+                                                        <div class="font-size-xlg text-muted">
+                                                            <small class="opacity-5 pr-1">$</small>
+                                                            <span>54</span>
+                                                            <small class="text-success pl-2">
+                                                                <i class="fa fa-angle-up"></i>
+                                                            </small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
