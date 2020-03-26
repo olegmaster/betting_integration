2a0e7468143241d49e32790c0fb9e083c35b0e93
oleg-divine <oleg.web.pro@gmail.com>
2020-03-26 08:53:31 +0200

    add skeleton

---
+++ resources/views/user/buy_key.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.cabinet')
+
+@section('title', 'Купить ключ')
+
+
+
+@section('content')
+    <div class="app-page-title">
+        <div class="page-title-wrapper">
+            <div class="page-title-heading">
+                <div class="page-title-icon">
+                    <i class="fa fa-user-circle ">
+                    </i>
+                </div>
+                <div>
+                    <div class="page-title-subheading">Информация о пользователе
+                    </div>
+                </div>
+            </div>
+            <div class="page-title-actions">
+
+            </div>
+        </div>
+    </div>
+@endsection
