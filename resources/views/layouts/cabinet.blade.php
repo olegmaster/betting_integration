c3c58c77c4e9f78306c0a94b770b9cf93a5b999f
oleg-divine <oleg.web.pro@gmail.com>
2020-03-30 07:15:37 +0300

    corrected menu

--- resources/views/layouts/cabinet.blade.php
+++ resources/views/layouts/cabinet.blade.php
@@ -71,28 +71,31 @@
                                 <div class="btn-group">
                                     <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
                                        class="p-0 btn">
-                                        <img width="42" class="rounded-circle" src="/uploads/{{\Illuminate\Support\Facades\Auth::user()->avatar ?? ''}}" alt="">
+                                        <img width="42" class="rounded-circle"
+                                             src="/uploads/{{\Illuminate\Support\Facades\Auth::user()->avatar ?? ''}}"
+                                             alt="">
                                         <i class="fa fa-angle-down ml-2 opacity-8"></i>
                                     </a>
                                     <div tabindex="-1" role="menu" aria-hidden="true"
                                          class="dropdown-menu dropdown-menu-right">
-                                        <button type="button" tabindex="0" class="dropdown-item">User Account</button>
+                                        <button type="button" tabindex="0" class="dropdown-item">
+                                            <a class="dropdown-item" href="{{url('/admin/profile')}}">
+                                                {{ __('Профиль') }}
+                                            </a>
+                                        </button>
                                         <button type="button" tabindex="0" class="dropdown-item">
 
                                             <a class="dropdown-item" href="{{ route('logout') }}"
                                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
+                                                {{ __('Выйти') }}
                                             </a>
 
-                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                  style="display: none;">
                                                 @csrf
                                             </form>
                                         </button>
-                                        <h6 tabindex="-1" class="dropdown-header">Header</h6>
-                                        <button type="button" tabindex="0" class="dropdown-item">Actions</button>
-                                        <div tabindex="-1" class="dropdown-divider"></div>
-                                        <button type="button" tabindex="0" class="dropdown-item">Dividers</button>
                                     </div>
                                 </div>
                             </div>
@@ -100,14 +103,6 @@
                                 <div class="widget-heading">
                                     {{\Illuminate\Support\Facades\Auth::user()->full_name}}
                                 </div>
-                                <div class="widget-subheading">
-                                    VP People Manager
-                                </div>
-                            </div>
-                            <div class="widget-content-right header-user-info ml-3">
-                                <button type="button" class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
-                                    <i class="fa text-white fa-calendar pr-1 pl-1"></i>
-                                </button>
                             </div>
                         </div>
                     </div>
