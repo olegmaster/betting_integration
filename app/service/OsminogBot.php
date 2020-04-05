dc76c6e3b6d525d4f08b0831a11a6a48e49739e0
oleg-divine <oleg.web.pro@gmail.com>
2020-04-05 21:34:46 +0300

    bug fix

--- app/service/OsminogBot.php
+++ app/service/OsminogBot.php
@@ -3,6 +3,8 @@
 namespace App\service;
 
 
+use Egulias\EmailValidator\Exception\ExpectingCTEXT;
+use Illuminate\Support\Facades\Log;
 use Psy\Util\Str;
 
 class OsminogBot
@@ -12,13 +14,17 @@ class OsminogBot
 
     public function __construct()
     {
-        $this->dbn = ibase_connect(
-            config('app.bot_firebird_db'),
-            config('app.bot_db_username'),
-            config('app.bot_db_password')
-        );
-        $hash = \Illuminate\Support\Str::random(32);
-        $this->hash = $hash;
+        try {
+            $this->dbn = ibase_connect(
+                config('app.bot_firebird_db'),
+                config('app.bot_db_username'),
+                config('app.bot_db_password')
+            );
+            $hash = \Illuminate\Support\Str::random(32);
+            $this->hash = $hash;
+        } catch (\Exception $e) {
+            Log::emergency('unable connect to firebird of bot');
+        }
     }
 
     /**
@@ -30,11 +36,14 @@ class OsminogBot
      */
     public function authorize()
     {
-
-        //echo $this->hash;die;
-        $sql = "select * from AUTORIZE('sitebot','sitebot', '$this->hash' )";
-        $rc = ibase_query($this->dbn, $sql);
-        return ibase_free_result($rc);
+        try {
+            $sql = "select * from AUTORIZE('sitebot','sitebot', '$this->hash' )";
+            $rc = ibase_query($this->dbn, $sql);
+            $response = ibase_free_result($rc);
+            Log::debug($response);
+        } catch (\Exception $e) {
+            Log::emergency('unable to authorize osminog bot');
+        }
     }
 
     /**
@@ -51,9 +60,15 @@ class OsminogBot
      */
     public function addKey($login, $password)
     {
-        $sql = "select * from ADD_KEY_FROM_SITE('$this->hash', '$login', '$password')";
-        $rc = ibase_query($this->dbn, $sql);
-        return $rc;
+        try {
+            $sql = "select * from ADD_KEY_FROM_SITE('$this->hash', '$login', '$password')";
+            $rc = ibase_query($this->dbn, $sql);
+            $res = ibase_free_result($rc);
+            Log::debug($res);
+        } catch (\Exception $e) {
+            Log::emergency('unable add a key to osminog bot');
+        }
+
     }
 
     /**
@@ -74,9 +89,14 @@ class OsminogBot
      */
     public function updateKey($login, $password, $status, $addMinutes = 0)
     {
-        $sql = "select * from SET_USER_STATE('$this->hash', '$login', '$password', $status, $addMinutes)";
-        $rc = ibase_query($this->dbn, $sql);
-        return $rc;
+        try {
+            $sql = "select * from SET_USER_STATE('$this->hash', '$login', '$password', $status, $addMinutes)";
+            $rc = ibase_query($this->dbn, $sql);
+            $res = ibase_free_result($rc);
+            Log::debug($res);
+        } catch (\Exception $exception) {
+            Log::emergency('unable to update key of osminogbot');
+        }
     }
 
 }
