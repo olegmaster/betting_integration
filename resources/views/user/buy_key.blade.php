854406366421b5f432a3d6ba14cb108138d04845
oleg-divine <oleg.web.pro@gmail.com>
2020-04-16 22:57:36 +0300

    replaced icon

--- resources/views/user/buy_key.blade.php
+++ resources/views/user/buy_key.blade.php
@@ -9,7 +9,7 @@
         <div class="page-title-wrapper">
             <div class="page-title-heading">
                 <div class="page-title-icon">
-                    <i class="pe-7s-photo-gallery">
+                    <i class="pe-7s-credit">
                     </i>
                 </div>
                 <div>Купить ключ
