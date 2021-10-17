9b4c7e8a915142c4214cceaf7b60eac3a743dec8
Oleg Pristashkin <oleg.web.pro.master@gmail.com>
2021-10-17 11:46:14 +0300

    refactoring: middleware

--- routes/web.php
+++ routes/web.php
@@ -35,58 +35,67 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::namespace('Admin')->prefix('admin')->group(function () {
-    Route::get('/', [SummaryController::class, 'summary'])->name('summary');
 
-    Route::get('transaction-history', [TransactionController::class, 'transactions'])->name('atrans');
+    Route::middleware(['auth', 'admin'])->group(function () {
+        Route::get('/', [SummaryController::class, 'summary'])->name('summary');
 
-    Route::get('bot-download', [BotController::class, 'bot']);
-    Route::post('bot-save', [BotController::class, 'botSave']);
+        Route::get('transaction-history', [TransactionController::class, 'transactions'])->name('atrans');
 
-    Route::get('help', [HelpController::class, 'help'])->name('ahelp');
-    Route::post('/admin-help-store', [HelpController::class, 'helpStore']);
+        Route::get('bot-download', [BotController::class, 'bot']);
+        Route::post('bot-save', [BotController::class, 'botSave']);
 
-    Route::get('user-card/{id}', [UserCardController::class, 'userCard']);
+        Route::get('help', [HelpController::class, 'help'])->name('ahelp');
+        Route::post('/admin-help-store', [HelpController::class, 'helpStore']);
 
-    Route::post('update-user-profile/{id}', [ProfileController::class, 'updateUserProfile']);
-    Route::get('profile', [ProfileController::class, 'profile']);
+        Route::get('user-card/{id}', [UserCardController::class, 'userCard']);
 
-    Route::get('login-as/{id}', [AdminController::class, 'loginAs']);
-    Route::post('/update-admin-avatar', [AdminController::class, 'updateAdminAvatar']);
+        Route::post('update-user-profile/{id}', [ProfileController::class, 'updateUserProfile']);
+        Route::get('profile', [ProfileController::class, 'profile']);
 
-    Route::post('login', [AdminLoginController::class, 'login']);
+        Route::get('login-as/{id}', [AdminController::class, 'loginAs']);
+        Route::post('/update-admin-avatar', [AdminController::class, 'updateAdminAvatar']);
 
-    Route::get('login', function () {
-        return view('auth.admin-login');
+        Route::post('login', [AdminLoginController::class, 'login']);
+
+        Route::get('login', function () {
+            return view('auth.admin-login');
+        });
+
+        Route::get('users', [UserController::class, 'users'])->name('ausers');
+        Route::get('change-status-activate/{id}', [UserController::class, 'changeUserStatusActivate']);
+        Route::get('change-status-deactivate/{id}', [UserController::class, 'changeUserStatusDeactivate']);
+        Route::post('update-user-password/{id}', [UserController::class, 'updateUserPassword']);
+        Route::post('/update-admin-password', [UserController::class, 'updatePassword']);
+
+        Route::get('keys', [KeyController::class, 'keys'])->name('akeys');
+        Route::get('freeze-key/{id}', [KeyController::class, 'freezeKey']);
+        Route::get('unfreeze-key/{id}', [KeyController::class, 'unFreezeKey']);
+        Route::get('delete-key/{id}', [KeyController::class, 'deleteKey']);
+        Route::get('activate-key/{id}', [KeyController::class, 'activateKey']);
+        Route::get('deactivate-key/{id}', [KeyController::class, 'deActivateKey']);
+        Route::get('long-key/{id}', [KeyController::class, 'longKey']);
     });
 
-    Route::get('users', [UserController::class, 'users'])->name('ausers');
-    Route::get('change-status-activate/{id}', [UserController::class, 'changeUserStatusActivate']);
-    Route::get('change-status-deactivate/{id}', [UserController::class, 'changeUserStatusDeactivate']);
-    Route::post('update-user-password/{id}', [UserController::class, 'updateUserPassword']);
-    Route::post('/update-admin-password', [UserController::class, 'updatePassword']);
 
-    Route::get('keys', [KeyController::class, 'keys'])->name('akeys');
-    Route::get('freeze-key/{id}', [KeyController::class, 'freezeKey']);
-    Route::get('unfreeze-key/{id}', [KeyController::class, 'unFreezeKey']);
-    Route::get('delete-key/{id}', [KeyController::class, 'deleteKey']);
-    Route::get('activate-key/{id}', [KeyController::class, 'activateKey']);
-    Route::get('deactivate-key/{id}', [KeyController::class, 'deActivateKey']);
-    Route::get('long-key/{id}', [KeyController::class, 'longKey']);
 });
 
 
 Route::namespace('Cabinet')->prefix('cabinet')->group(function () {
-    Route::get('keys', 'UserController@keys')->name('ukeys');
-    Route::get('buy-key', 'UserController@buyKey')->name('bkey');
-    Route::post('buy-key-handle', 'UserController@buyKeyHandle');
-    Route::get('download-bot', 'UserController@downloadBot');
-    Route::get('setup', 'UserController@setup')->name('usetup');
-    Route::post('setup-update', 'UserController@setupUpdate');
-    Route::get('help', 'UserController@help')->name('uhelp');
-    Route::get('profile', 'UserController@profile')->name('uprofile');
-    Route::post('profile-update', 'UserController@profileUpdate');
-    Route::post('update-user-avatar', 'UserController@updateUserAvatar');
-    Route::post('update-user-password', 'UserController@updatePassword');
+
+    Route::middleware(['auth', 'active'])->group(function () {
+        Route::get('keys', 'UserController@keys')->name('ukeys');
+        Route::get('buy-key', 'UserController@buyKey')->name('bkey');
+        Route::post('buy-key-handle', 'UserController@buyKeyHandle');
+        Route::get('download-bot', 'UserController@downloadBot');
+        Route::get('setup', 'UserController@setup')->name('usetup');
+        Route::post('setup-update', 'UserController@setupUpdate');
+        Route::get('help', 'UserController@help')->name('uhelp');
+        Route::get('profile', 'UserController@profile')->name('uprofile');
+        Route::post('profile-update', 'UserController@profileUpdate');
+        Route::post('update-user-avatar', 'UserController@updateUserAvatar');
+        Route::post('update-user-password', 'UserController@updatePassword');
+    });
+
 });
 
 
